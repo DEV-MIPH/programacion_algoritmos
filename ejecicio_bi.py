3d120c97e3d9f1c236eb742b0ef028d8f0e391b1
MisterPoro <104047318+MisterPoro@users.noreply.github.com>
2022-06-06 10:31:02 -0400

    Add files via upload

---
+++ ejecicio_bi.py
@@ -0,0 +1,16 @@
+import numpy as np 
+import random
+
+
+matriz = np.diag([1,1,1])
+print(matriz)
+
+for i in range(3):
+    for j in range(3):
+        print(matriz[i][j])
+        
+#llenar con numeros aleatoreos
+for i in range(3):
+    for j in range(3):
+        (matriz[i][j]) = random.randint(0,100)
+print(matriz)
